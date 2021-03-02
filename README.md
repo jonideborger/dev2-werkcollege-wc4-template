638b4063b8297ac1ebec2c445af180b753d942ea
Joni De Borger <joni.de.borger@ehb.be>
2021-03-03 00:42:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dev2-werkcollege-template
+template dev2 (20-21)
+
+### Temp notes
+
+Verwijder ``js/data.js`` wanneer overbodig.
+Extra javascript modules zoals ``js/utils.js`` mogen steeds worden toegevoegd.
+
+Use arrow functions starting from WC2?
---
+++ js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//First implement the 'initFields' function
+//Afterwards, add a 'getWeather' function that accepts an argument 'city'
+//This function should call the OpenWeatherMap-API, URL:
+//http://api.openweathermap.org/data/2.5/weather?q=__CITY__&APPID=d7b955c4c268fe54649d6f0d702b39d1&units=metric
+//replace '__CITY__' with your 'city' variable
+
+const weatherApp = {
+    initFields() {
+        console.log('Init your events here');
+    }
+}
+
+weatherApp.initFields();
